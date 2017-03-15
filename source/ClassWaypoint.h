c6b5bd9ee9c214975527ea7ba34e0db5b25f3431
Max <maximilian.mussotter@gmx.de>
2017-03-15 22:25:41 +0100

    addition to previous commit, added Class file

---
+++ source/ClassWaypoint.h
@@ -0,0 +1,27 @@
+#ifndef WAYPOINT_H
+#define WAYPOINT_H
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//		Introduce class "Waypoint"
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+class Waypoint{
+	public:
+		/* //constructors ... */
+		/* Student(string nam, string num ) { name=nam; id=num; } */
+		Waypoint(){}
+		Waypoint(std::string title, double distance ) { WaypointTitle = title; WaypointDistance = distance; }
+
+		// setters ...
+		void setTitle(std::string title){ WaypointTitle = title; }
+		void setDistance(double distance) { WaypointDistance = daypoints; }
+
+		// getters ...
+		std::string getTitle() { return WaypointTitle; }
+		double getDistance() { return WaypointDistance; }
+	private:
+		std::string WaypointTitle; // add any needed info here, just like in a C/C++ 'struct'
+		double WaypointDistance;	
+};
+#endif
